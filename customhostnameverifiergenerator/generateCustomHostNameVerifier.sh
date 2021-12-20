b7214b95668a1586860a064bfe5868a7ef0f9cbf
gnsuryan <gurudutt.suryanarayana@oracle.com>
2021-12-20 14:25:33 +0000

    restructuring length directory structure

--- customhostnameverifiergenerator/generateCustomHostNameVerifier.sh
+++ customhostnameverifiergenerator/generateCustomHostNameVerifier.sh
@@ -61,32 +61,32 @@ OUTPUT_DIR="$SCRIPT_DIR/output"
 mkdir -p "$OUTPUT_DIR"
 
 echo "Copying HostNames Template file ..."
-cp -rf $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValuesTemplate.txt $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
+cp -rf $SCRIPT_DIR/src/main/java/HostNameValuesTemplate.txt $SCRIPT_DIR/src/main/java/HostNameValues.java
 
 cd $SCRIPT_DIR/src/main/java
 echo "Compiling Default HostNameValues.java ..."
-$JAVA_HOME/bin/javac -d $CLASSES_DIR com/oracle/azure/weblogic/HostNameValues.java
+$JAVA_HOME/bin/javac -d $CLASSES_DIR HostNameValues.java
 
 echo "Compiling WebLogicCustomHostNameVerifier.java "
-$JAVA_HOME/bin/javac -d $CLASSES_DIR -classpath $WL_HOME/server/lib/weblogic.jar:$CLASSES_DIR com/oracle/azure/weblogic/security/util/WebLogicCustomHostNameVerifier.java
+$JAVA_HOME/bin/javac -d $CLASSES_DIR -classpath $WL_HOME/server/lib/weblogic.jar:$CLASSES_DIR WebLogicCustomHostNameVerifier.java
 
 echo "generating weblogicustomhostnameverifier.jar"
 cd $CLASSES_DIR
 jar cf $OUTPUT_DIR/weblogicustomhostnameverifier.jar com/oracle/azure/weblogic/security/util/*.class
 
 #replace arg values in HostNameValues.java
-cp  $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java.bak
-sed -i "s/debugEnabled=.*/debugEnabled=${debugFlag};/g" $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
-sed -i "s/adminInternalHostName=.*/adminInternalHostName=\"${adminInternalHostName}\";/g" $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
-sed -i "s/adminExternalHostName=.*/adminExternalHostName=\"${adminExternalHostName}\";/g" $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
-sed -i "s/adminDNSZoneName=.*/adminDNSZoneName=\"${adminDNSZoneName}\";/g" $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
-sed -i "s/dnsLabelPrefix=.*/dnsLabelPrefix=\"${dnsLabelPrefix}\";/g" $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
-sed -i "s/wlsDomainName=.*/wlsDomainName=\"${wlsDomainName}\";/g" $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
-sed -i "s/azureResourceGroupRegion=.*/azureResourceGroupRegion=\"${azureResourceGroupRegion}\";/g" $SCRIPT_DIR/src/main/java/com/oracle/azure/weblogic/HostNameValues.java
+cp  $SCRIPT_DIR/src/main/java/HostNameValues.java $SCRIPT_DIR/src/main/java/HostNameValues.java.bak
+sed -i "s/debugEnabled=.*/debugEnabled=${debugFlag};/g" $SCRIPT_DIR/src/main/java/HostNameValues.java
+sed -i "s/adminInternalHostName=.*/adminInternalHostName=\"${adminInternalHostName}\";/g" $SCRIPT_DIR/src/main/java/HostNameValues.java
+sed -i "s/adminExternalHostName=.*/adminExternalHostName=\"${adminExternalHostName}\";/g" $SCRIPT_DIR/src/main/java/HostNameValues.java
+sed -i "s/adminDNSZoneName=.*/adminDNSZoneName=\"${adminDNSZoneName}\";/g" $SCRIPT_DIR/src/main/java/HostNameValues.java
+sed -i "s/dnsLabelPrefix=.*/dnsLabelPrefix=\"${dnsLabelPrefix}\";/g" $SCRIPT_DIR/src/main/java/HostNameValues.java
+sed -i "s/wlsDomainName=.*/wlsDomainName=\"${wlsDomainName}\";/g" $SCRIPT_DIR/src/main/java/HostNameValues.java
+sed -i "s/azureResourceGroupRegion=.*/azureResourceGroupRegion=\"${azureResourceGroupRegion}\";/g" $SCRIPT_DIR/src/main/java/HostNameValues.java
 
 cd $SCRIPT_DIR/src/main/java
 echo "Compiling modified HostNameValues.java ..."
-$JAVA_HOME/bin/javac -d $CLASSES_DIR com/oracle/azure/weblogic/HostNameValues.java
+$JAVA_HOME/bin/javac -d $CLASSES_DIR HostNameValues.java
 
 echo "generating hostnamevalues.jar"
 cd $CLASSES_DIR
@@ -105,9 +105,9 @@ find $CLASSES_DIR -type f -name "*.class" -delete
 
 echo "Running HostNameVerifierTest ..."
 cd $SCRIPT_DIR/src/test/java
-$JAVA_HOME/bin/javac -d $CLASSES_DIR -classpath $OUTPUT_DIR/hostnamevalues.jar:$OUTPUT_DIR/weblogicustomhostnameverifier.jar com/oracle/azure/weblogic/security/test/WebLogicCustomHostNameVerifierTest.java
+$JAVA_HOME/bin/javac -d $CLASSES_DIR -classpath $OUTPUT_DIR/hostnamevalues.jar:$OUTPUT_DIR/weblogicustomhostnameverifier.jar WebLogicCustomHostNameVerifierTest.java
 
-$JAVA_HOME/bin/java -classpath $CLASSES_DIR:$OUTPUT_DIR/hostnamevalues.jar:$OUTPUT_DIR/weblogicustomhostnameverifier.jar com/oracle/azure/weblogic/security/test/WebLogicCustomHostNameVerifierTest "$@"
+$JAVA_HOME/bin/java -classpath $CLASSES_DIR:$OUTPUT_DIR/hostnamevalues.jar:$OUTPUT_DIR/weblogicustomhostnameverifier.jar com.oracle.azure.weblogic.security.test.WebLogicCustomHostNameVerifierTest "$@"
 
 if [ $? != 0 ];
 then
